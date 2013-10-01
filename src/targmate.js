5b732583d1c14e065a86218a06a13ad9196973ab
civoic <civoic@gmail.com>
2013-10-01 17:22:02 +0800

    support queue

--- src/targmate.js
+++ src/targmate.js
@@ -1,36 +1,105 @@
 (function($) {
+    var queueImpl = {
+        add: function(node, queuedFunc) {
+            var newFunc = function(){
+                queuedFunc();
+                $(this).dequeue();
+            };
+
+            node.queue(newFunc);
+        },
+    };
+
+    var animateImpl = {
+        base: function(srcNode, animateQueue) {
+
+            if (Object.prototype.toString.call(animateQueue) != '[object Array]') {
+                return;
+            }
+
+            for (var item in animateQueue) {
+                queueImpl.add(srcNode, animateQueue[item]);
+            }
+        },
+        start: function(node) {
+            node.dequeue();
+        },
+
+        direct: function(srcNode, dstNode) {
+            var cloneNode = $(srcNode).clone();
+            var startPos = $(srcNode).offset();
+            var endPos = dstNode.offset();
+            var dstWidth = dstNode.width();
+
+            $("body").append(cloneNode);
+            cloneNode.css("position", "absolute");
+            cloneNode.css("top", startPos.top);
+            cloneNode.css("left", startPos.left);
+            var funcs = [
+                function() {
+                    cloneNode.animate({top:endPos.top, left:endPos.left, width: dstWidth});
+                   
+                },
+                function() {
+                     cloneNode.fadeOut(3000);
+                }
+            ];
+
+            animateImpl.base(cloneNode, funcs);
+            animateImpl.start(cloneNode);
+        },
+        jump: function(srcNode, dstNode) {
+            var cloneNode = $(srcNode).clone();
+            var startPos = $(srcNode).offset();
+            var endPos = dstNode.offset();
+            var dstWidth = dstNode.width();
+
+            $("body").append(cloneNode);
+            cloneNode.css("position", "absolute");
+            cloneNode.css("top", startPos.top);
+            cloneNode.css("left", startPos.left);
+            cloneNode.animate({top:endPos.top, left:endPos.left, width: dstWidth}).fadeOut("slow");
+        },
+    };
 
 	$.fn.extend({
 
         targmate: function(options) {
+            
 
+            /*paramters in options
+            type:direct, jump, fall, custom
+            postions:array of the points which define the line of the animation, only used when type is custom
+            */
         	var defaults = {
-                
+                type : "direct",
             };
 
             var options = $.extend(defaults, options);
 
             var sourceNode = $(this);
             var targetNode = $("#target_node");
+
             sourceNode.click(function(){
-            	var cloneNode = $(this).clone();
 
-            	var sourcePos = $(this).offset();
-            	var sourceTop = sourcePos.top;
-            	var sourceLeft = sourcePos.left;
+                if (options.type == "direct") {
+                    animateImpl.direct(sourceNode, targetNode);
+                }
+                else if (options.type == "jump") {
+                    animateImpl.direct(sourceNode, targetNode);
+                }
+                else if (options.type == "fall") {
 
-            	var targetPos = targetNode.offset();
-            	var targetTop = targetPos.top;
-            	var targetLeft = targetPos.left;
+                }
+                else if (options.type == "custom") {
 
-            	var firstLeft = Math.abs(targetLeft - sourceLeft)/2;
-				var targetNodeWidth = targetNode.width();
+                }
+                else {
+                    // wrong, do nothing
+                    return;
+                }
 
-            	$("body").append(cloneNode);
-            	cloneNode.css("position", "absolute");
-            	cloneNode.css("top", sourceTop);
-            	cloneNode.css("left", sourceLeft);
-            	cloneNode.animate({top:sourceTop - 100, left:sourceLeft + firstLeft}).animate({width:targetNodeWidth, top:targetTop, left:targetLeft}).fadeOut("slow");
+            	
             });
 
         }
